827481720f77e19b80c879f36fb6f958f19a08f2
samuellmfa <samuellmfa@gmail.com>
2023-06-22 14:52:21 +0200

    login integrated

---
+++ src/pages/_app.jsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { CookiesProvider } from 'react-cookie';
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <CookiesProvider>
+      <Component {...pageProps} />
+    </CookiesProvider>
+  );
+}
