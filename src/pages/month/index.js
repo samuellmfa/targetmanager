87186dd138f49e189f8770457ca937cf71528bd9
samuellmfa <samuellmfa@gmail.com>
2023-07-07 07:20:01 +0200

    deployment fix

--- src/pages/month/index.js
+++ src/pages/month/index.js
@@ -7,7 +7,6 @@ import _footer from "../component/Footer/footer";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.css";
-import ProductList from "../../../components/ProductList";
 import React, { useState } from "react";
 export default  function Employee( {username, created} )
 {
